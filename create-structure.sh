083d5d86848f27188956fb3879611c6b315075ae
GT <GT@NB-MROTGER.local>
2019-11-24 19:30:58 -0300

    Reordenando script

--- create-structure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p src/actions src/assets/fonts src/assets/images src/assets/styles src/assets/uploads src/components src/config/constants src/config/routes src/config/store src/containers src/reducers src/services src/utils/doc src/utils/lib src/utils/tests
-
-touch src/config/constants/actionTypes.js src/utils/doc/rules.md src/utils/doc/structure.md src/utils/doc/tips.md
---
+++ i_love_redux/create-react-redux-structure-snoop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p prueba/src/actions prueba/src/assets/fonts prueba/src/assets/images prueba/src/assets/styles prueba/src/assets/uploads prueba/src/components prueba/src/config/constants prueba/src/config/routes prueba/src/config/store prueba/src/containers prueba/src/reducers prueba/src/services prueba/src/utils/doc prueba/src/utils/lib prueba/src/utils/tests
+
+touch prueba/src/config/constants/actionTypes.js prueba/src/utils/doc/rules.md prueba/src/utils/doc/structure.md prueba/src/utils/doc/tips.md
